a7d16802bd21085bd959b55aee940f50ef320453
nicola <n.pettinaroli@4sigma.it>
2026-02-26 17:18:50 +0100

    First migrations

--- server/platform/platform_api/models.py
+++ server/platform/platform_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Sport(models.Model):
+    key = models.SlugField(max_length=32, unique=True)
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.key} ({self.name})"
--- server/verticals/volley/volley_api/models.py
+++ server/verticals/volley/volley_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=128)
+    city = models.CharField(max_length=64, blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
