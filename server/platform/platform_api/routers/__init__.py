ae804d1d072586c0ef2024abdd303028cb17cc06
nicola <n.pettinaroli@4sigma.it>
2026-03-09 16:56:27 +0100

    Using BaseRoute in the platform app

---
+++ server/platform/platform_api/routers/__init__.py
@@ -0,0 +1,12 @@
+from shared.api_contract.routing import BaseRoute, TableUrlConfig
+
+from platform_api.models.entities import Sport
+from platform_api.models.geo import Country, GeoPlace
+
+
+class PlatformRoute(BaseRoute):
+    TABLES_URLS = {
+        Country: TableUrlConfig("core", "countries", "iso2"),
+        Sport: TableUrlConfig("core", "sports", "key"),
+        GeoPlace: TableUrlConfig("geo", "locations"),
+    }
